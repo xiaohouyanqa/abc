0562a6f79cefb7ad400704f14103d71c6ec4231e
houyan <houyan_uestc@163.com>
2020-07-19 22:47:28 +0800

    hha

--- src/git/main.java
+++ src/git/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("2");
     }
 }
